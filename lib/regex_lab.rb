a3515aad60db61552f6fcde05ff56c5fc7b75aac
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-02 18:06:46 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
     return false
   else
     return true
-  end 
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -11,7 +11,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.scan(/\s\w{5}\s/)
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
