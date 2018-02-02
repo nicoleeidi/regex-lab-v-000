c98b990fea3a51d850e9c727713ec26bcdb88061
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-02 17:49:31 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-  word.grep(/\b[aeiou]/)
+  word.match(/\b[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-   text.grep(/\bun\w*ing\b/)
+   text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-  text.grep(/\s\w{5}\s/)
+  text.scan(/\s\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.grep(/\b[A-Z]\w*\W\b/)
+  text.match(/\b[A-Z]\w*\W\b/)
 end
 
 def valid_phone_number?(phone)
-  phone.grep(/[0-9]{10}/)
+  phone.match(/[0-9]{10}/)
 end
